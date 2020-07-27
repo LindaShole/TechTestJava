68c30f46dcb23ae185fd3f1d7d111573d6dad120
Linda.Motholo <Linda.Motholo@investec.co.za>
2020-07-27 14:26:33 +0200

    Tech Test proj commit

---
+++ anycompany/src/test/java/za/co/anycompany/Main.java
@@ -0,0 +1,10 @@
+package za.co.anycompany;
+
+import za.co.anycompany.model.Order;
+import za.co.anycompany.service.OrderService;
+
+public class Main {
+
+    public static void main(String[] args) {
+    }
+}
