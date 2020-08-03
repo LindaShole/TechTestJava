40e6ed23b865c939980527d80b63a1e2e67d9a50
daGuyu91 <66088739+daGuyu91@users.noreply.github.com>
2020-08-03 08:58:24 +0200

    Update OrderRepository.java
    Changed the implementation

--- anycompany/src/main/java/za.co.anycompany/datalayer/OrderRepository.java
+++ anycompany/src/main/java/za.co.anycompany/datalayer/OrderRepository.java
@@ -4,53 +4,14 @@ import za.co.anycompany.model.Order;
 
 import java.sql.*;
 
-public class OrderRepository {
+@Repository
+public class OrderRepository extends JpaRepository<Booking, Integer> {
+ 
 
-    private static final String DB_DRIVER = "org.h2.Driver";
-    private static final String DB_CONNECTION = "jdbc:h2:mem:test;DB_CLOSE_DELAY=-1";
-    private static final String DB_USER = "";
-    private static final String DB_PASSWORD = "";
-
-    public void save(Order order) {
-        Connection connection = getDBConnection();
-        Statement statement = null;
-        PreparedStatement preparedStatement = null;
-
-        try {
-            statement = connection.createStatement();
-            statement.executeUpdate("CREATE TABLE ORDER (oderId int primary key not null, amount number(10,2), vat number (3,1))");
-            connection.prepareStatement("INSERT INTO ORDER(oderId, amount, vat) VALUES(?,?,?)");
-            preparedStatement.setInt(1, order.getOrderId());
-            preparedStatement.setDouble(2, order.getAmount());
-            preparedStatement.setDouble(3, order.getVAT());
-            preparedStatement.executeUpdate();
-
-        } catch (SQLException e) {
-
-        } finally {
-            try {
-                statement.close();
-                preparedStatement.close();
-                connection.close();
-            } catch (SQLException e) {
-                System.out.println(e.getMessage());
-            }
-        }
-    }
-
-    private static Connection getDBConnection() {
-        Connection dbConnection = null;
-        try {
-            Class.forName(DB_DRIVER);
-        } catch (ClassNotFoundException e) {
-            System.out.println(e.getMessage());
-        }
-        try {
-            dbConnection = DriverManager.getConnection(DB_CONNECTION, DB_USER, DB_PASSWORD);
-            return dbConnection;
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
-        }
-        return dbConnection;
-    }
+    @Query(value = "SELECT * FROM Order co WHERE " +
+            "EXISTS (SELECT 1 FROM customer c WHERE co.name = c.id ) " +
+            "AND EXISTS (SELECT 1 FROM Order o WHERE o.orderId = o.orderId AND c.name IN :name)",
+            nativeQuery = true)
+    List<Order> queryBy(@Param("name") String name,
+                            @Param("amount") List<String> amount);
 }
