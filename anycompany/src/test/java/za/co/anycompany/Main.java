d4408d4266e83d1378299ae5f1dc388a4f03ec5a
daGuyu91 <66088739+daGuyu91@users.noreply.github.com>
2020-08-03 09:38:47 +0200

    Update Main.java

--- anycompany/src/test/java/za/co/anycompany/Main.java
+++ anycompany/src/test/java/za/co/anycompany/Main.java
@@ -1,8 +1,12 @@
 package za.co.anycompany;
 
 
+1
+@SpringBootApplication
+
 public class Main {
 
     public static void main(String[] args) {
+         SpringApplication.run(SpringBootMainClassApplication.class, args);
     }
 }
