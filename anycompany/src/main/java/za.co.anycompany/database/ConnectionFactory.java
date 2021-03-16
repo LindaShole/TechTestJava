7aa443a35d89b28b8274294275c872454ef51d94
Nathi360 <nmothoa360@gmamil.com>
2021-03-16 11:10:19 +0200

    Added db connection factory

---
+++ anycompany/src/main/java/za.co.anycompany/database/ConnectionFactory.java
@@ -0,0 +1,26 @@
+package za.co.anycompany.database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class ConnectionFactory {
+
+    private static final String DB_DRIVER = "org.h2.Driver";
+    private static final String DB_CONNECTION = "jdbc:h2:mem:test;DB_CLOSE_DELAY=-1";
+    private static final String DB_USER = "";
+    private static final String DB_PASSWORD = "";
+
+    public static Connection getDBConnection() {
+        Connection dbConnection = null;
+        try {
+            Class.forName(DB_DRIVER);
+            dbConnection = DriverManager.getConnection(DB_CONNECTION, DB_USER, DB_PASSWORD);
+        } catch (ClassNotFoundException | SQLException e) {
+            Logger.getAnonymousLogger().log(Level.SEVERE, e.getMessage());
+        }
+        return dbConnection;
+    }
+}
