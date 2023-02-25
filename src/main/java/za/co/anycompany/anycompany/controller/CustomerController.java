e6f629eddf1d15aff4c687bde5bb50f89a253521
Xolisani Mthimunye <Xolisani.Mthimunye@investec.co.za>
2023-02-25 11:52:25 +0200

    feat: add the methods for the CustomerController

---
+++ src/main/java/za/co/anycompany/anycompany/controller/CustomerController.java
@@ -0,0 +1,67 @@
+package za.co.anycompany.anycompany.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+import za.co.anycompany.anycompany.datalayer.CustomerRepository;
+import za.co.anycompany.anycompany.datalayer.OrderRepository;
+import za.co.anycompany.anycompany.model.Customer;
+import za.co.anycompany.anycompany.model.Order;
+import za.co.anycompany.anycompany.service.CustomerService;
+
+import java.util.List;
+
+@RestController
+public class CustomerController {
+    @Autowired
+    CustomerService customerService;
+
+    public CustomerController(CustomerService customerService){
+        this.customerService = customerService;
+    }
+
+    // http://localhost:8080/customers
+    @GetMapping("/customers")
+    private List<Customer> getAllCustomers() throws Exception{
+        return customerService.getAllCustomers();
+    }
+
+    //http://localhost:8080/customers/{id}
+    @GetMapping("/customers/{id}")
+    private Customer getCustomer(@PathVariable int id){
+        return customerService.getCustomerById(id);
+    }
+
+    //http://localhost:8080/customers/{id}
+    @DeleteMapping("/customers/{id}")
+    private void deleteCustomer(@PathVariable int id){
+        customerService.delete(id);
+    }
+
+    // http://localhost:8080/customers
+    @PostMapping("/customers")
+    private int saveCustomer(@RequestBody Customer customer){
+        return  customer.getCustomerid();
+    }
+
+    // http://localhost:8080/customers
+    @PutMapping("/customers")
+    private Customer update(@RequestBody Customer customer){
+        customerService.saveOrUpdate(customer);
+        return customer;
+    }
+
+/*
+    @GetMapping("/customer/{id}")
+    public Customer getCustomerById(@PathVariable int id) throws Exception{
+        return customerService.findById(id);
+    }
+
+    @GetMapping("/customer/{id}")
+    public Customer get(@PathVariable int id){
+        Customer customer = customerService.get(id);
+        if (order==null) throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        return order;
+    } */
+}
