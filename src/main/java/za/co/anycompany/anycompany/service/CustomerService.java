86a15e4e27f187aca9fd7c93694dcf949bba3e6d
Xolisani Mthimunye <Xolisani.Mthimunye@investec.co.za>
2023-02-25 12:07:25 +0200

    feat: add the methods for the CUstomerService

---
+++ src/main/java/za/co/anycompany/anycompany/service/CustomerService.java
@@ -0,0 +1,38 @@
+package za.co.anycompany.anycompany.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import za.co.anycompany.anycompany.datalayer.CustomerRepository;
+import za.co.anycompany.anycompany.model.Customer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class CustomerService {
+    @Autowired
+    private CustomerRepository customerRepository;
+//    private Customer customer;
+
+    public List<Customer> getAllCustomers() {
+        List<Customer> customers = new ArrayList<Customer>();
+        customerRepository.findAll().forEach(customers1 -> customers.add(customers1));
+        return customers;
+    }
+
+    public Customer getCustomerById(int id) {
+        return customerRepository.findById(id).get();
+    }
+
+    public void delete(int id) {
+        customerRepository.deleteById(id);
+    }
+
+    public void saveOrUpdate(Customer customer) {
+        customerRepository.save(customer);
+    }
+
+    public void update(Customer customer, int id){
+        customerRepository.save(customer);
+    }
+}
