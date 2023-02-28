15aac48db726bd81973b4358f3c4a6913aaa4406
Xolisani Mthimunye <Xolisani.Mthimunye@investec.co.za>
2023-03-01 01:51:09 +0200

    feat: add new files for exception handling on the orders

---
+++ src/main/java/za/co/anycompany/anycompany/web/exception/OrderNotFoundException.java
@@ -0,0 +1,20 @@
+package za.co.anycompany.anycompany.web.exception;
+
+public class OrderNotFoundException extends RuntimeException {
+
+    public OrderNotFoundException() {
+        super();
+    }
+
+    public OrderNotFoundException(final String message, final Throwable cause) {
+        super(message, cause);
+    }
+
+    public OrderNotFoundException(final String message) {
+        super(message);
+    }
+
+    public OrderNotFoundException(final Throwable cause) {
+        super(cause);
+    }
+}
