fa069014adfe2b0abe85c21c5c8b2517a21fa1cc
Xolisani Mthimunye <Xolisani.Mthimunye@investec.co.za>
2023-03-07 19:45:30 +0200

    refactor: clean up the test code

--- src/main/java/za/co/anycompany/anycompany/controller/OrderController.java
+++ src/main/java/za/co/anycompany/anycompany/controller/OrderController.java
@@ -20,16 +20,15 @@ public class OrderController {
         this.orderService = orderService;
     }
 
-    // 1. http://localhost:8081/orders
+    // 1.1 http://localhost:8081/orders
     @GetMapping("/orders")
     public String get(@RequestParam(name="name", required=false, defaultValue="User") String name, Model model){
         List<Order> orders = orderService.getAllOrders();
         model.addAttribute("orders", orders);
-
         return "orders";
     }
 
-    // 2. http://localhost:8081/orders/{id}
+    // 1.2 http://localhost:8081/orders/{id}
     @GetMapping("/orders/{id}")
     public String get(@PathVariable int id, @RequestParam(name="name", required=false, defaultValue="Xolisani") String name, Model model){
         //model.addAttribute("name", name);
@@ -38,20 +37,19 @@ public class OrderController {
         model.addAttribute("amount", order.getAmount());
         model.addAttribute("VAT", order.getVAT());
         model.addAttribute("customerId", order.getCustomerId());
-       /* return "customer"; // return form
-
-        Order order = orderService.get(id);
+       /* Order order = orderService.get(id);
         if (order==null) throw new ResponseStatusException(HttpStatus.NOT_FOUND);*/
         return "order";
     }
 
+    // 1.3 http://localhost:8081/orders/{id}/{customerId}
     @GetMapping("/orders/{id}/customer/{customerId}")
     public String getCustomerOrders(@PathVariable int customerId, @RequestParam(name="name", required=false, defaultValue="Xolisani") String name, Model model){
         //model.addAttribute("name", name);
         List <Order> orders = orderService.getOrderByCustomerId(customerId);
         model.addAttribute("orders", orders);
-        //Customer customer = new Customer();
 
+        //Customer customer = new Customer();
       /*  model.addAttribute("orderId", order.getOrderId());
         model.addAttribute("amount", order.getAmount());
         model.addAttribute("VAT", order.getVAT());
@@ -63,14 +61,7 @@ public class OrderController {
         return "orders";
     }
 
-    // 3. http://localhost:8081/orders/{id}
-    @DeleteMapping("/orders/{id}")
-    public void delete(@PathVariable Integer id){
-        Order order = orderService.remove(id);
-        if (order==null) throw new ResponseStatusException(HttpStatus.NOT_FOUND);
-    }
-
-    // 4.0. http://localhost:8081/order "get the form"
+    // 1.4 http://localhost:8081/order
     @GetMapping("/order")
     public String orderHere(Model model){
         // get one order
@@ -84,29 +75,33 @@ public class OrderController {
         return "place";
     }
 
-    // 4.1 http://localhost:8081/orders
+    // 2.1 http://localhost:8081/orders
     @PostMapping("/orders")
-    public String create(@RequestBody Order order){ //@Valid
-        //int customerId = order.getCustomerId();
-
+    public String create(@RequestBody Order order){
         order.setAmount(1165.36);
         order.setOrderId(4);
-        //Integer.parseInt(UUID.randomUUID().toString())
         orderService.placeOrder( order, 11);
         return "orders";
     }
 
-    // 4.1 http://localhost:8081/orders
+    // 2.2 http://localhost:8081/orders
     @PostMapping("/order")
     public String orderPlaced(@ModelAttribute("order") Order order){
         return "ordered";
     }
 
-    // 5. http://localhost:8081/orders/{id}  *test the POST HERE*
+    // 2.3 http://localhost:8081/orders/{id}  *test the POST HERE*
     @PostMapping("/myorders")
     public void placeOrder(@RequestBody Order order){
         order.setAmount(299);
-       // order.setCustomerId(11);
+        // order.setCustomerId(11);
         orderService.placeOrder(order, 11);
+    }
+
+    // 3. http://localhost:8081/orders/{id}
+    @DeleteMapping("/orders/{id}")
+    public void delete(@PathVariable Integer id){
+        Order order = orderService.remove(id);
+        if (order==null) throw new ResponseStatusException(HttpStatus.NOT_FOUND);
     }
 }
